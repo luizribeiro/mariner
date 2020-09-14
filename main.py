61d2acc382274c3888e00343a833fc655c808fe1
Luiz Ribeiro <luizribeiro@gmail.com>
2020-09-14 00:10:08 -0400

    Basic printer status on response

--- main.py
+++
@@ -1,8 +0,0 @@
-from mariner.mars import ElegooMars
-
-
-elegoo_mars = ElegooMars()
-elegoo_mars.open()
-print(f"Firmware Version: {elegoo_mars.get_firmware_version()}")
-print(f"State: {elegoo_mars.get_state()}")
-elegoo_mars.close()
--- mariner/server.py
+++ mariner/server.py
@@ -11,9 +11,14 @@ def hello() -> str:
     try:
         elegoo_mars = ElegooMars()
         elegoo_mars.open()
-        return "Hello, World!"
-    except Exception:
-        return "Error"
+
+        response = "Hello, World!\n"
+        response += f"Firmware Version: {elegoo_mars.get_firmware_version()}\n"
+        response += f"State: {elegoo_mars.get_state()}\n"
+
+        return response.replace("\n", "<br />")
+    except Exception as ex:
+        return f"Error: {str(ex)}<br />"
     finally:
         try:
             elegoo_mars.close()
