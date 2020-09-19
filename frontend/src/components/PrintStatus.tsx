373ba6074eac2cdad782c409e275f33c0ff2c077
Luiz Ribeiro <luizribeiro@gmail.com>
2020-09-19 16:59:33 -0400

    Added network request to PrintStatus

--- frontend/src/components/PrintStatus.tsx
+++ frontend/src/components/PrintStatus.tsx
@@ -4,105 +4,165 @@ import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import { makeStyles } from "@material-ui/core/styles";
+import { createStyles, WithStyles, withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import PauseIcon from "@material-ui/icons/Pause";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import axios, { AxiosResponse } from "axios";
+import nullthrows from "nullthrows";
 import React from "react";
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 100,
-    maxWidth: 400,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  playButton: {
-    padding: 6,
-  },
-  playIcon: {
-    height: 38,
-    width: 38,
-  },
-  gridRoot: {
-    flexGrow: 1,
-    padding: 24,
-    paddingBottom: 12,
-    textAlign: "center",
-  },
-});
+const styles = () =>
+  createStyles({
+    root: {
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+    playButton: {
+      padding: 6,
+    },
+    playIcon: {
+      height: 38,
+      width: 38,
+    },
+    gridRoot: {
+      flexGrow: 1,
+      padding: 24,
+      paddingBottom: 12,
+      textAlign: "center",
+    },
+  });
 
-export type PrintStatusProps = {
+export interface PrintStatusState {
+  isLoading: boolean;
+  data?: {
+    isPrinting: boolean;
+    selectedFile: string;
+    progress: number;
+  };
+}
+
+interface PrintStatusAPIResponse {
+  selected_file: string;
+  is_printing: boolean;
   progress: number;
-};
+}
 
-export default function PrintStatus(
-  props: PrintStatusProps
-): React.ReactElement {
-  const classes = useStyles();
-  const { progress } = props;
+class PrintStatus extends React.Component<
+  WithStyles<typeof styles>,
+  PrintStatusState
+> {
+  state: PrintStatusState = {
+    isLoading: true,
+  };
 
-  return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textPrimary" gutterBottom>
-          Elegoo Mars Pro
-        </Typography>
-        <Box display="flex">
-          <Box paddingRight={1}>
-            <IconButton className={classes.playButton} aria-label="play/pause">
-              <PlayArrowIcon className={classes.playIcon} />
-            </IconButton>
-          </Box>
-          <Box width="100%">
-            <Typography component="h6" variant="h6" color="textSecondary">
-              lattice.ctb
-            </Typography>
-            <Box display="flex" alignItems="center">
-              <Box width="100%" mr={1}>
-                <LinearProgress variant="determinate" value={progress} />
-              </Box>
-              <Box minWidth={35}>
-                <Typography variant="body2" color="textSecondary">
-                  {`${Math.round(progress)}%`}
-                </Typography>
+  async componentDidMount(): Promise<void> {
+    const response: AxiosResponse<PrintStatusAPIResponse> = await axios.get(
+      "print_status"
+    );
+    this.setState({
+      isLoading: false,
+      data: {
+        isPrinting: response.data.is_printing,
+        progress: response.data.progress,
+        selectedFile: response.data.selected_file,
+      },
+    });
+  }
+
+  _renderButton(): React.ReactElement {
+    const { classes } = this.props;
+    const { isPrinting } = nullthrows(this.state.data);
+
+    const icon = isPrinting ? (
+      <PauseIcon className={classes.playIcon} />
+    ) : (
+      <PlayArrowIcon className={classes.playIcon} />
+    );
+
+    return (
+      <IconButton className={classes.playButton} aria-label="play/pause">
+        {icon}
+      </IconButton>
+    );
+  }
+
+  render(): React.ReactElement | null {
+    const { classes } = this.props;
+
+    if (this.state.isLoading) {
+      return null;
+    }
+
+    const { progress, selectedFile } = nullthrows(this.state.data);
+
+    return (
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textPrimary"
+            gutterBottom
+          >
+            Elegoo Mars Pro
+          </Typography>
+          <Box display="flex">
+            <Box paddingRight={1}>{this._renderButton()}</Box>
+            <Box width="100%">
+              <Typography component="h6" variant="h6" color="textSecondary">
+                {selectedFile}
+              </Typography>
+              <Box display="flex" alignItems="center">
+                <Box width="100%" mr={1}>
+                  <LinearProgress variant="determinate" value={progress} />
+                </Box>
+                <Box minWidth={35}>
+                  <Typography variant="body2" color="textSecondary">
+                    {`${Math.round(progress)}%`}
+                  </Typography>
+                </Box>
               </Box>
             </Box>
           </Box>
-        </Box>
-        <div className={classes.gridRoot}>
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <Typography variant="h4" color="textPrimary" display="inline">
-                2h44&nbsp;
-              </Typography>
-              <Typography
-                variant="body1"
-                color="textSecondary"
-                display="inline"
-              >
-                left
-              </Typography>
+          <div className={classes.gridRoot}>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <Typography variant="h4" color="textPrimary" display="inline">
+                  2h44&nbsp;
+                </Typography>
+                <Typography
+                  variant="body1"
+                  color="textSecondary"
+                  display="inline"
+                >
+                  left
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="h4" color="textPrimary" display="inline">
+                  53
+                </Typography>
+                <Typography variant="h5" color="textPrimary" display="inline">
+                  /79&nbsp;
+                </Typography>
+                <Typography
+                  variant="body1"
+                  color="textSecondary"
+                  display="inline"
+                >
+                  layers
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h4" color="textPrimary" display="inline">
-                53
-              </Typography>
-              <Typography variant="h5" color="textPrimary" display="inline">
-                /79&nbsp;
-              </Typography>
-              <Typography
-                variant="body1"
-                color="textSecondary"
-                display="inline"
-              >
-                layers
-              </Typography>
-            </Grid>
-          </Grid>
-        </div>
-      </CardContent>
-    </Card>
-  );
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
 }
+
+export default withStyles(styles)(PrintStatus);
--- frontend/src/components/stories/PrintStatus.stories.tsx
+++ frontend/src/components/stories/PrintStatus.stories.tsx
@@ -1,21 +1,47 @@
 import { Story } from "@storybook/react";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import React from "react";
-import type { PrintStatusProps } from "../PrintStatus";
 import PrintStatus from "../PrintStatus";
+
+const axiosMock = new MockAdapter(axios);
 
 export default {
   title: "Example/PrintStatus",
   component: PrintStatus,
   argTypes: {
+    isPrinting: { control: "boolean" },
     progress: { control: { type: "number", min: 0, max: 100 } },
+    selectedFile: { control: "text" },
   },
 };
 
-const Template: Story<PrintStatusProps> = (args: PrintStatusProps) => (
-  <PrintStatus {...args} />
-);
+const Template: Story = (args) => {
+  axiosMock.onGet("print_status").reply(200, {
+    selected_file: args.selectedFile,
+    is_printing: args.isPrinting,
+    progress: args.progress,
+  });
+  return <PrintStatus />;
+};
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Printing = Template.bind({});
+Printing.args = {
+  isPrinting: true,
+  selectedFile: "lattice.ctb",
   progress: 20.0,
+};
+
+export const Paused = Template.bind({});
+Paused.args = {
+  isPrinting: false,
+  selectedFile: "lattice.ctb",
+  progress: 20.0,
+};
+
+export const Stopped = Template.bind({});
+Stopped.args = {
+  isPrinting: false,
+  selectedFile: "lattice.ctb",
+  progress: 0.0,
 };
