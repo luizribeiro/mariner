28ee2c5ceb4786b586136b8098be1fd7ec80b637
Luiz Ribeiro <luizribeiro@gmail.com>
2020-09-20 14:42:35 -0400

    [frontend] Set basename on BrowserRouter correctly

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,9 +5,10 @@ import { BrowserRouter } from "react-router-dom";
 import Main from "./components/Main";
 
 const wrapper = document.getElementById("container");
+const basename = window.location.pathname.split("/").slice(0, -1).join("/");
 wrapper
   ? ReactDOM.render(
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Main />
       </BrowserRouter>,
       wrapper
