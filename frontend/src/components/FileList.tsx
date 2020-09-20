212e547500c239768643e3213a34517382023eda
Luiz Ribeiro <luizribeiro@gmail.com>
2020-09-20 08:48:14 -0400

    [frontend] Added a FileList component

---
+++ frontend/src/components/FileList.tsx
@@ -0,0 +1,74 @@
+import Avatar from "@material-ui/core/Avatar";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import LayersIcon from "@material-ui/icons/Layers";
+import PrintIcon from "@material-ui/icons/Print";
+import axios, { AxiosResponse } from "axios";
+import nullthrows from "nullthrows";
+import React from "react";
+
+export interface FileListState {
+  isLoading: boolean;
+  data?: FileListAPIResponse;
+}
+
+interface FileListAPIResponse {
+  files: [
+    {
+      filename: string;
+    }
+  ];
+}
+
+export default class FileList extends React.Component<{}, FileListState> {
+  state: FileListState = {
+    isLoading: true,
+  };
+
+  async componentDidMount(): Promise<void> {
+    const response: AxiosResponse<FileListAPIResponse> = await axios.get(
+      "api/file_list"
+    );
+    this.setState({
+      isLoading: false,
+      data: response.data,
+    });
+  }
+
+  render(): React.ReactElement | null {
+    if (this.state.isLoading) {
+      return null;
+    }
+
+    const { files } = nullthrows(this.state.data);
+    const listItems = files.map((file) => (
+      <ListItem key={file.filename}>
+        <ListItemAvatar>
+          <Avatar>
+            <LayersIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={file.filename} />
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="print">
+            <PrintIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    ));
+
+    return (
+      <Card>
+        <CardContent>
+          <List>{listItems}</List>
+        </CardContent>
+      </Card>
+    );
+  }
+}
---
+++ frontend/src/components/stories/FileList.stories.tsx
@@ -0,0 +1,43 @@
+import { Story } from "@storybook/react";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import React from "react";
+import FileList from "../FileList";
+
+const axiosMock = new MockAdapter(axios);
+
+export default {
+  title: "FileList",
+  component: FileList,
+  argTypes: {
+    isPrinting: { control: "boolean" },
+    progress: { control: { type: "number", min: 0, max: 100 } },
+    selectedFile: { control: "text" },
+  },
+};
+
+const Template: Story = (_args) => {
+  axiosMock.onGet("api/file_list").reply(200, {
+    files: [
+      { filename: "z-axis-stabilizer.ctb" },
+      { filename: "case v2.ctb" },
+      { filename: "lattice.ctb" },
+      { filename: "pi zero case 2.ctb" },
+      { filename: "pi zero case.ctb" },
+      { filename: "pi stem case.ctb" },
+      { filename: "_Ark-chitu.ctb" },
+      { filename: "_Ark-3.ctb" },
+      { filename: "ark-base.ctb" },
+      { filename: "rake-that-doesnt-break.ctb" },
+      { filename: "sd card case.ctb" },
+      { filename: "_rake_in_the_lake_-_rake.ctb" },
+      { filename: "rake-no-gods-no-masters.ctb" },
+      { filename: "Busbar-cover.ctb" },
+      { filename: "Busbar-cover-again.ctb" },
+      { filename: "LittleBBC.ctb" },
+    ],
+  });
+  return <FileList />;
+};
+
+export const Default = Template.bind({});
