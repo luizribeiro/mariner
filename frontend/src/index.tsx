875f248c050018d02931f41e7f2c5cc24e879599
Luiz Ribeiro <luizribeiro@gmail.com>
2020-09-26 10:42:04 -0400

    [frontend] Removed unnecessary basename from HashRouter

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,10 +5,9 @@ import { HashRouter } from "react-router-dom";
 import Main from "./components/Main";
 
 const wrapper = document.getElementById("container");
-const basename = window.location.pathname.split("/").slice(0, -1).join("/");
 wrapper
   ? ReactDOM.render(
-      <HashRouter basename={basename}>
+      <HashRouter>
         <Main />
       </HashRouter>,
       wrapper
