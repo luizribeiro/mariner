ef1f84c5cbfdc3e4ce862c27a3a3f5b41c383dbd
Luiz Ribeiro <luizribeiro@gmail.com>
2020-09-26 16:44:19 -0400

    [frontend] Make items on FileList fully clickable

--- frontend/src/components/FileList.tsx
+++ frontend/src/components/FileList.tsx
@@ -1,14 +1,11 @@
 import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import LayersIcon from "@material-ui/icons/Layers";
-import PrintIcon from "@material-ui/icons/Print";
 import axios, { AxiosResponse } from "axios";
 import nullthrows from "nullthrows";
 import React from "react";
@@ -30,30 +27,25 @@ function FileListItem({ file }: { file: FileAPIResponse }): React.ReactElement {
   const printTime = renderTime(file.print_time_secs);
   const history = useHistory();
   return (
-    <ListItem key={file.filename}>
+    <ListItem button key={file.filename} onClick={handleClickOpen}>
       <ListItemAvatar>
         <Avatar>
           <LayersIcon />
         </Avatar>
       </ListItemAvatar>
       <ListItemText primary={file.filename} secondary={printTime} />
-      <ListItemSecondaryAction>
-        <IconButton edge="end" aria-label="print" onClick={handleClickOpen}>
-          <PrintIcon />
-        </IconButton>
-        <FileDetailsDialog
-          filename={file.filename}
-          onCancel={handleClose}
-          onClose={handleClose}
-          onPrint={async () => {
-            await startPrint(file.filename);
-            setOpen(false);
-            history.push("/");
-          }}
-          open={open}
-          scroll="paper"
-        />
-      </ListItemSecondaryAction>
+      <FileDetailsDialog
+        filename={file.filename}
+        onCancel={handleClose}
+        onClose={handleClose}
+        onPrint={async () => {
+          await startPrint(file.filename);
+          setOpen(false);
+          history.push("/");
+        }}
+        open={open}
+        scroll="paper"
+      />
     </ListItem>
   );
 }
