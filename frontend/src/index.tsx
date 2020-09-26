1d44949413d71aec767b506a8339dad04b034bfe
Luiz Ribeiro <luizribeiro@gmail.com>
2020-09-26 10:15:44 -0400

    [frontend] Use HashRouter instead of BrowserRouter so refreshing works

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,16 @@
 import "@babel/polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Main from "./components/Main";
 
 const wrapper = document.getElementById("container");
 const basename = window.location.pathname.split("/").slice(0, -1).join("/");
 wrapper
   ? ReactDOM.render(
-      <BrowserRouter basename={basename}>
+      <HashRouter basename={basename}>
         <Main />
-      </BrowserRouter>,
+      </HashRouter>,
       wrapper
     )
   : false;
