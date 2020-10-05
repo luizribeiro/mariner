600d39608372ee4a24658755ddcb23b3be3ca71b
Luiz Ribeiro <luizribeiro@gmail.com>
2020-10-04 23:03:58 -0400

    Fixed build on Travis

--- mariner/server/utils.py
+++ mariner/server/utils.py
@@ -7,5 +7,9 @@ def get_frontend_assets_path() -> str:
         Path("./frontend/dist/"),
         Path("/opt/venvs/mariner3d/assets/"),
     ]
-    path = next(path for path in potential_paths if path.exists() and path.is_dir())
+    try:
+        path = next(path for path in potential_paths if path.exists() and path.is_dir())
+    except StopIteration:
+        # fallback to potential_paths. we're likely running from tests
+        path = potential_paths[0]
     return str(path.absolute())
