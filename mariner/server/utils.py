ca565891735be72a033825076e492be1a9618db7
Luiz Ribeiro <luizribeiro@gmail.com>
2020-10-04 23:24:08 -0400

    [deb] Moved assets under dist directory of install path

--- mariner/server/utils.py
+++ mariner/server/utils.py
@@ -5,7 +5,7 @@ from typing import Sequence
 def get_frontend_assets_path() -> str:
     potential_paths: Sequence[Path] = [
         Path("./frontend/dist/"),
-        Path("/opt/venvs/mariner3d/assets/"),
+        Path("/opt/venvs/mariner3d/dist/"),
     ]
     try:
         path = next(path for path in potential_paths if path.exists() and path.is_dir())
