b36d399a20f044fc203884ac2322b5d342a4676c
Luiz Ribeiro <luizribeiro@gmail.com>
2021-04-16 23:26:15 -0500

    Move get_file_format to mariner.file_formats.utils

--- mariner/server/utils.py
+++ mariner/server/utils.py
@@ -1,15 +1,12 @@
 import io
 import os
-from typing import Mapping, Type
 
 import png
 from flask_caching import Cache
 
 from mariner.config import FILES_DIRECTORY
 from mariner.file_formats import SlicedModelFile
-from mariner.file_formats.ctb import CTBFile
-from mariner.file_formats.cbddlp import CBDDLPFile
-from mariner.file_formats.fdg import FDGFile
+from mariner.file_formats.utils import get_file_format
 from mariner.server.app import app
 
 
@@ -19,7 +16,7 @@ cache = Cache(app)
 @cache.memoize(timeout=0)
 def read_cached_sliced_model_file(filename: str) -> SlicedModelFile:
     assert os.path.isabs(filename)
-    file_format = _get_file_format(filename)
+    file_format = get_file_format(filename)
     return file_format.read(FILES_DIRECTORY / filename)
 
 
@@ -27,23 +24,7 @@ def read_cached_sliced_model_file(filename: str) -> SlicedModelFile:
 def read_cached_preview(filename: str) -> bytes:
     assert os.path.isabs(filename)
     bytes = io.BytesIO()
-    file_format = _get_file_format(filename)
+    file_format = get_file_format(filename)
     preview_image: png.Image = file_format.read_preview(FILES_DIRECTORY / filename)
     preview_image.write(bytes)
     return bytes.getvalue()
-
-
-def _get_file_format(filename: str) -> Type[SlicedModelFile]:
-    (_, extension) = os.path.splitext(filename)
-
-    # only reason this is in here is because it makes mocking on
-    # mariner.tests.test_server simpler
-    EXTENSION_TO_FILE_FORMAT: Mapping[str, Type[SlicedModelFile]] = {
-        ".ctb": CTBFile,
-        ".cbddlp": CBDDLPFile,
-        ".fdg": FDGFile,
-    }
-    file_format = EXTENSION_TO_FILE_FORMAT.get(extension.lower())
-
-    assert file_format is not None
-    return file_format
