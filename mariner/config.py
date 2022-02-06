9c277615ed7ffa0f998cd892a61cb86e15dcbd70
fcollingwood <37040349+fcollingwood@users.noreply.github.com>
2022-02-06 18:02:13 +1300

    Update config.py

--- mariner/config.py
+++ mariner/config.py
@@ -45,7 +45,7 @@ def get_file_sort_order() -> str:
 
 def get_show_hidden_files() -> bool:
     file_list = _get_config().get("file_list")
-    default_show = bool(true)
+    default_show = True
     if not isinstance(file_list, dict):
         return default_show
     return str(file_list.get("show_hidden", default_show))
